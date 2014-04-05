a16cb6036e228bfb966d0c10d829363fda3f3680
Jesse Luehrs <doy@tozt.net>
2014-04-05 02:38:46 -0400

    display keys when pressed

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 #include "runes.h"
 
@@ -11,12 +11,24 @@ int main (int argc, char *argv[])
     cairo_select_font_face(t->cr, "monospace", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(t->cr, 14.0);
     cairo_set_source_rgb(t->cr, 0.0, 0.0, 1.0);
-    cairo_move_to(t->cr, 10.0, 50.0);
-    cairo_show_text(t->cr, "Hello, world");
+    cairo_move_to(t->cr, 0.0, 14.0);
 
-    runes_term_flush(t);
+    XSelectInput(t->w->dpy, t->w->w, KeyPressMask|KeyReleaseMask);
 
-    sleep(2);
+    for (;;) {
+        XEvent e;
+        char buf[10];
+
+        XNextEvent(t->w->dpy, &e);
+        if (e.type == KeyPress) {
+            XLookupString(&e, buf, 10, NULL, NULL);
+        }
+        else {
+            continue;
+        }
+        cairo_show_text(t->cr, buf);
+        runes_term_flush(t);
+    }
 
     runes_term_destroy(t);
 
