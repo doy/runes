42406a59119b5f03fbf5b4a09b3cdb0d6f3c72f3
Jesse Luehrs <doy@tozt.net>
2014-04-11 17:17:28 -0400

    refactor

--- vt100.c
+++ vt100.c
@@ -15,6 +15,7 @@ static const char *ctrl_chars =
 static char *runes_vt100_handle_ctrl_char(RunesTerm *t, char *buf, size_t len);
 static char *runes_vt100_handle_escape_sequence(
     RunesTerm *t, char *buf, size_t len);
+static char *runes_vt100_handle_csi(RunesTerm *t, char *buf, size_t len);
 static void runes_vt100_unhandled_escape_sequence(
     RunesTerm *t, int *p, char type);
 
@@ -84,6 +85,7 @@ static char *runes_vt100_handle_ctrl_char(RunesTerm *t, char *buf, size_t len)
         break;
     }
     case '\033':
+        buf++;
         buf = runes_vt100_handle_escape_sequence(t, buf, len);
         break;
     default: {
@@ -98,172 +100,177 @@ static char *runes_vt100_handle_ctrl_char(RunesTerm *t, char *buf, size_t len)
 static char *runes_vt100_handle_escape_sequence(
     RunesTerm *t, char *buf, size_t len)
 {
+    switch (buf[0]) {
+    case '[': /* CSI */
+        buf++;
+        buf = runes_vt100_handle_csi(t, buf, len);
+        break;
+    default:
+        runes_vt100_unhandled_escape_sequence(t, NULL, buf[0]);
+        buf++;
+        break;
+    }
+
+    return buf;
+}
+
+static char *runes_vt100_handle_csi(RunesTerm *t, char *buf, size_t len)
+{
+    int p[3] = { -1, -1, -1 };
+    int paramlen;
+    char type;
+
     UNUSED(len);
 
-    if (buf[1] == '[') { /* CSI */
-        int p[3] = { -1, -1, -1 };
-        int paramlen;
-        char type;
+    /* XXX stop hardcoding the max number of parameters */
+    if (sscanf(buf, "%d;%d;%d%c%n", &p[0], &p[1], &p[2], &type, &paramlen) == 4) {
+        /* nothing */
+    }
+    else if (sscanf(buf, "%d;%d%c%n", &p[0], &p[1], &type, &paramlen) == 3) {
+        /* nothing */
+    }
+    else if (sscanf(buf, "%d%c%n", &p[0], &type, &paramlen) == 2) {
+        /* nothing */
+    }
+    else if (sscanf(buf, "%c%n", &type, &paramlen) == 1) {
+        /* nothing */
+    }
 
-        buf += 2;
+    switch (type) {
+    case 'D': { /* CUB */
+        int row, col;
 
-        /* XXX stop hardcoding the max number of parameters */
-        if (sscanf(buf, "%d;%d;%d%c%n", &p[0], &p[1], &p[2], &type, &paramlen) == 4) {
-            /* nothing */
-        }
-        else if (sscanf(buf, "%d;%d%c%n", &p[0], &p[1], &type, &paramlen) == 3) {
-            /* nothing */
-        }
-        else if (sscanf(buf, "%d%c%n", &p[0], &type, &paramlen) == 2) {
-            /* nothing */
-        }
-        else if (sscanf(buf, "%c%n", &type, &paramlen) == 1) {
-            /* nothing */
-        }
+        runes_display_get_position(t, &row, &col);
+        runes_display_move_to(t, row, col - 1);
+        break;
+    }
+    case 'B': { /* CUD */
+        int row, col;
 
-        switch (type) {
-        case 'D': { /* CUB */
-            int row, col;
+        runes_display_get_position(t, &row, &col);
+        runes_display_move_to(t, row + 1, col);
+        break;
+    }
+    case 'C': { /* CUF */
+        int row, col;
 
-            runes_display_get_position(t, &row, &col);
-            runes_display_move_to(t, row, col - 1);
+        runes_display_get_position(t, &row, &col);
+        runes_display_move_to(t, row, col + 1);
+        break;
+    }
+    case 'A': { /* CUU */
+        int row, col;
+
+        runes_display_get_position(t, &row, &col);
+        runes_display_move_to(t, row - 1, col);
+        break;
+    }
+    case 'H':   /* CUP */
+        if (p[0] == -1) {
+            p[0] = 0;
+        }
+        if (p[1] == -1) {
+            p[1] = 0;
+        }
+        runes_display_move_to(t, p[0], p[1]);
+        break;
+    case 'J':   /* ED */
+        switch (p[0]) {
+        case -1:
+        case 0:
+            runes_display_clear_screen_forward(t);
             break;
-        }
-        case 'B': { /* CUD */
-            int row, col;
-
-            runes_display_get_position(t, &row, &col);
-            runes_display_move_to(t, row + 1, col);
+        case 1:
+            /* XXX */
+            runes_vt100_unhandled_escape_sequence(t, p, type);
             break;
-        }
-        case 'C': { /* CUF */
-            int row, col;
-
-            runes_display_get_position(t, &row, &col);
-            runes_display_move_to(t, row, col + 1);
+        case 2:
+            runes_display_clear_screen(t);
             break;
-        }
-        case 'A': { /* CUU */
-            int row, col;
-
-            runes_display_get_position(t, &row, &col);
-            runes_display_move_to(t, row - 1, col);
-            break;
-        }
-        case 'H':   /* CUP */
-            if (p[0] == -1) {
-                p[0] = 0;
-            }
-            if (p[1] == -1) {
-                p[1] = 0;
-            }
-            runes_display_move_to(t, p[0], p[1]);
-            break;
-        case 'J':   /* ED */
-            switch (p[0]) {
-            case -1:
-            case 0:
-                runes_display_clear_screen_forward(t);
-                break;
-            case 1:
-                /* XXX */
-                runes_vt100_unhandled_escape_sequence(t, p, type);
-                break;
-            case 2:
-                runes_display_clear_screen(t);
-                break;
-            default:
-                runes_vt100_unhandled_escape_sequence(t, p, type);
-                break;
-            }
-            break;
-        case 'K':   /* EL */
-            switch (p[0]) {
-            case -1:
-            case 0:
-                runes_display_kill_line_forward(t);
-                break;
-            case 1:
-                /* XXX */
-                runes_vt100_unhandled_escape_sequence(t, p, type);
-                break;
-            case 2:
-                /* XXX */
-                runes_vt100_unhandled_escape_sequence(t, p, type);
-                break;
-            default:
-                runes_vt100_unhandled_escape_sequence(t, p, type);
-                break;
-            }
-            break;
-        case 'm': { /* SGR */
-            int i;
-
-            if (p[0] == -1) {
-                p[0] = 0;
-            }
-
-            for (i = 0; i < 3; ++i) {
-                switch (p[i]) {
-                case -1:
-                    break;
-                case 0:
-                    runes_display_reset_text_attributes(t);
-                    break;
-                case 1:
-                    runes_display_set_bold(t);
-                    break;
-                case 3:
-                    runes_display_set_italic(t);
-                    break;
-                case 4:
-                    runes_display_set_underline(t);
-                    break;
-                case 22:
-                    runes_display_reset_bold(t);
-                    break;
-                case 23:
-                    runes_display_reset_italic(t);
-                    break;
-                case 24:
-                    runes_display_reset_underline(t);
-                    break;
-                case 30: case 31: case 32: case 33:
-                case 34: case 35: case 36: case 37:
-                    runes_display_set_fg_color(t, t->colors[p[i] - 30]);
-                    break;
-                case 39:
-                    runes_display_reset_fg_color(t);
-                    break;
-                case 40: case 41: case 42: case 43:
-                case 44: case 45: case 46: case 47:
-                    runes_display_set_bg_color(t, t->colors[p[i] - 40]);
-                    break;
-                case 49:
-                    runes_display_reset_bg_color(t);
-                    break;
-                /* XXX ... */
-                default:
-                    runes_vt100_unhandled_escape_sequence(t, p, type);
-                    break;
-                }
-            }
-            break;
-        }
         default:
             runes_vt100_unhandled_escape_sequence(t, p, type);
             break;
         }
+        break;
+    case 'K':   /* EL */
+        switch (p[0]) {
+        case -1:
+        case 0:
+            runes_display_kill_line_forward(t);
+            break;
+        case 1:
+            /* XXX */
+            runes_vt100_unhandled_escape_sequence(t, p, type);
+            break;
+        case 2:
+            /* XXX */
+            runes_vt100_unhandled_escape_sequence(t, p, type);
+            break;
+        default:
+            runes_vt100_unhandled_escape_sequence(t, p, type);
+            break;
+        }
+        break;
+    case 'm': { /* SGR */
+        int i;
 
-        buf += paramlen;
+        if (p[0] == -1) {
+            p[0] = 0;
+        }
+
+        for (i = 0; i < 3; ++i) {
+            switch (p[i]) {
+            case -1:
+                break;
+            case 0:
+                runes_display_reset_text_attributes(t);
+                break;
+            case 1:
+                runes_display_set_bold(t);
+                break;
+            case 3:
+                runes_display_set_italic(t);
+                break;
+            case 4:
+                runes_display_set_underline(t);
+                break;
+            case 22:
+                runes_display_reset_bold(t);
+                break;
+            case 23:
+                runes_display_reset_italic(t);
+                break;
+            case 24:
+                runes_display_reset_underline(t);
+                break;
+            case 30: case 31: case 32: case 33:
+            case 34: case 35: case 36: case 37:
+                runes_display_set_fg_color(t, t->colors[p[i] - 30]);
+                break;
+            case 39:
+                runes_display_reset_fg_color(t);
+                break;
+            case 40: case 41: case 42: case 43:
+            case 44: case 45: case 46: case 47:
+                runes_display_set_bg_color(t, t->colors[p[i] - 40]);
+                break;
+            case 49:
+                runes_display_reset_bg_color(t);
+                break;
+            /* XXX ... */
+            default:
+                runes_vt100_unhandled_escape_sequence(t, p, type);
+                break;
+            }
+        }
+        break;
     }
-    else {
-        runes_vt100_unhandled_escape_sequence(t, NULL, buf[1]);
-        /* do nothing, for now */
-        buf++;
+    default:
+        runes_vt100_unhandled_escape_sequence(t, p, type);
+        break;
     }
 
-    return buf;
+    return buf + paramlen;
 }
 
 static void runes_vt100_unhandled_escape_sequence(
